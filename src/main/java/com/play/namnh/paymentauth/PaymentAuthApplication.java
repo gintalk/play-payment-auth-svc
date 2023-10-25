822f518e02f4063420163541dc9a6bb9f4dd3bfe
nam.nguyen1 <nam.nguyen1@tyme.com>
2023-10-25 17:24:23 +0700

    no message

---
+++ src/main/java/com/play/namnh/paymentauth/PaymentAuthApplication.java
@@ -0,0 +1,12 @@
+package com.play.namnh.paymentauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentAuthApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PaymentAuthApplication.class, args);
+  }
+}
